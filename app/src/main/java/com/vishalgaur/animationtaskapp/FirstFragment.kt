0bc8fe4cb46bbf3117b17c4ee278916a526b4a61
i-vishi <rr.vishalgaur@gmail.com>
2021-06-02 11:34:03 +0530

    animation added to text views in card 1

--- app/src/main/java/com/vishalgaur/animationtaskapp/FirstFragment.kt
+++ app/src/main/java/com/vishalgaur/animationtaskapp/FirstFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AlphaAnimation
 import android.view.animation.TranslateAnimation
 import androidx.activity.OnBackPressedCallback
+import androidx.constraintlayout.widget.Group
 import androidx.fragment.app.Fragment
 import com.vishalgaur.animationtaskapp.databinding.FragmentFirstBinding
 
@@ -45,6 +47,8 @@ class FirstFragment : Fragment() {
 					when (currState) {
 						2 -> {
 							slideDownAnimation(binding.card2)
+							contentHideAnimation(binding.card1SummaryGroup, 0)
+							contentShowAnimation(binding.card1ContentGroup, 800)
 							binding.card1Btn.text = getString(R.string.card1_btn_text)
 							currState = 1
 						}
@@ -61,14 +65,19 @@ class FirstFragment : Fragment() {
 				}
 			})
 	}
+
 	private fun setViews() {
 		binding.card2.visibility = View.INVISIBLE
 		binding.card3.visibility = View.INVISIBLE
+		contentShowAnimation(binding.card1ContentGroup, 0)
+		binding.card1SummaryGroup.visibility = View.INVISIBLE
 		binding.card1Btn.text = getString(R.string.card1_btn_text)
 		binding.card1Btn.setOnClickListener {
 			currState = when (currState) {
 				1 -> {
 					slideUpAnimation(binding.card2)
+					contentHideAnimation(binding.card1ContentGroup, 0)
+					contentShowAnimation(binding.card1SummaryGroup, 800)
 					binding.card1Btn.text = getString(R.string.card2_btn_text)
 					2
 				}
@@ -99,6 +108,32 @@ class FirstFragment : Fragment() {
 		animate.duration = 500
 		view.startAnimation(animate)
 		view.visibility = View.GONE
+	}
+
+	private fun contentShowAnimation(viewGroup: Group, timeOffset: Int) {
+		val allViews = viewGroup.referencedIds.map { view?.findViewById<View>(it) }
+		val animate = AlphaAnimation(0F, 1F)
+		animate.duration = 750
+		animate.fillAfter = true
+		animate.startOffset = timeOffset + animate.startOffset
+		allViews.forEach {
+			it?.startAnimation(animate)
+		}
+//		viewGroup.startAnimation(animate)
+		viewGroup.visibility = View.VISIBLE
+	}
+
+	private fun contentHideAnimation(viewGroup: Group, timeOffset: Int) {
+		val allViews = viewGroup.referencedIds.map { view?.findViewById<View>(it) }
+		val animate = AlphaAnimation(1F, 0F)
+		animate.duration = 750
+		animate.fillAfter = true
+		animate.startOffset = timeOffset + animate.startOffset
+		allViews.forEach {
+			it?.startAnimation(animate)
+		}
+//		viewGroup.startAnimation(animate)
+		viewGroup.visibility = View.GONE
 	}
 
 	override fun onDestroyView() {
